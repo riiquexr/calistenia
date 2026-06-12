0bfff393121e4b04671b64337a1f7db7292754e6
GabrielInacio198 <gabrielinacio198@gmail.com>
2026-06-11 22:47:07 -0300

    Remove .vercel prebuilt output to force Vercel build

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DrQW9Dng.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/NEGO/Desktop/Clientes/Marcos - Calistenia Mulheres/quiz-creator-studio-main (1)/quiz-creator-studio-main/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-Boqt82Ta.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-Boqt82Ta.js" } }] }, "/": { filePath: "C:/Users/NEGO/Desktop/Clientes/Marcos - Calistenia Mulheres/quiz-creator-studio-main (1)/quiz-creator-studio-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BBlIg4xB.js"] } } });
-export {
-  tsrStartManifest
-};
